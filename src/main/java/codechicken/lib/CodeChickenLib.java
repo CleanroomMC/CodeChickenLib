e5eee289bee44a63cb18e8ab7c914fb4bb29d95e
arcade_kappa <gkappa@outlands.top>
2025-07-28 18:20:56 +0800

    Remove update URL and fingerprint

--- src/main/java/codechicken/lib/CodeChickenLib.java
+++ src/main/java/codechicken/lib/CodeChickenLib.java
@@ -22,7 +22,7 @@ import java.io.File;
 /**
  * Created by covers1624 on 12/10/2016.
  */
-@Mod (modid = CodeChickenLib.MOD_ID, name = CodeChickenLib.MOD_NAME, dependencies = "required-after:forge@[14.23.4.2718,);required-after:cleanroom@[0.3.13-alpha,)", acceptedMinecraftVersions = CodeChickenLib.MC_VERSION_DEP, certificateFingerprint = "f1850c39b2516232a2108a7bd84d1cb5df93b261", updateJSON = CodeChickenLib.UPDATE_URL)
+@Mod (modid = CodeChickenLib.MOD_ID, name = CodeChickenLib.MOD_NAME, dependencies = "required-after:forge@[14.23.4.2718,);required-after:cleanroom@[0.3.13-alpha,)", acceptedMinecraftVersions = CodeChickenLib.MC_VERSION_DEP)
 public class CodeChickenLib {
 
     public static final String MOD_ID = "codechickenlib";
